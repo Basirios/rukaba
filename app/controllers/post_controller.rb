c1abdc77915f5db61b461c319810d7b441db6346
dmitrii <timjoww@gmail.com>
2012-08-14 23:51:58 +0400

    misc#2

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -2,7 +2,8 @@ class PostController < ApplicationController
   def new
 	@params = params
 	@flow = Flow.find(params[:flow])
-	@p = @flow.post.create(:name=>params[:name], :subj=>params[:subject], :email=>params[:email], :body=>params[:message], :pass=>params[:password])
+	@p = @flow.posts.create(:name=>params[:name], :subj=>params[:subject], :email=>params[:email], :body=>params[:message], :pass=>params[:password])
+	@p.save
 	redirect_to :back
 	
   end
