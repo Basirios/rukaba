56be1f2d6e060dd4d091aacf033c7b0f354fe722
dmitrii <timjoww@gmail.com>
2012-08-16 16:25:08 +0400

    misc#5

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -6,7 +6,8 @@ class BoardController < ApplicationController
  	@params = params
 	@board = Board.where(:url=>params[:board]).first
 	@target = "/boards/#{params[:board]}/flows"
-	@flows = @board.flows.order("local")
+	@flows = @board.flows.order("pinned ASC","updated_at DESC")
+	@gposts = @board.posts
 	@i = 1
   end
 end
